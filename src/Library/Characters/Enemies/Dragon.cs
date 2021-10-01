a4c290aac78199eec294026cc73d6955126727c2
Santiago De Olivera <santiagodeolivera@gmail.com>
2021-10-01 19:23:26 -0300

    Increase Dragon VP

--- src/Library/Characters/Enemies/Dragon.cs
+++ src/Library/Characters/Enemies/Dragon.cs
@@ -4,7 +4,7 @@ namespace RoleplayGame
     {
         public Dragon(string name): base(name) {}
 
-        public override int VictoryPoints => 200;
+        public override int VictoryPoints => 10;
 
         public override int AttackValue => base.AttackValue + 100;
         public override int DefenseValue => base.DefenseValue + 100;
