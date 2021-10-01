6d7bd01e5bf2d8e93707693e1924773ea8031513
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2021-10-01 19:22:52 -0300

    fixed skeleton VP

--- src/Library/Characters/Enemies/Skeleton.cs
+++ src/Library/Characters/Enemies/Skeleton.cs
@@ -2,11 +2,10 @@ namespace RoleplayGame
 {
     public class Skeleton : Enemy
     {
-        public override int VictoryPoints { get; }
+        public override int VictoryPoints => 1;
 
         public Skeleton(string name) : base(name)
         {
-            this.VictoryPoints = 2;
             this.AddItem(new Sword());
             this.AddItem(new Helmet());
         }
