3a16dc4898bbf2fd2a5ecbc43d2792955bf7d3db
Bianca Luzzatto <bianca.luzzatto@correo.ucu.edu.uy>
2021-10-01 19:22:55 -0300

    New VP Golem

--- src/Library/Characters/Enemies/Golem.cs
+++ src/Library/Characters/Enemies/Golem.cs
@@ -2,13 +2,12 @@ namespace RoleplayGame
 {
     public class Golem: Enemy
     {
-        public override int VictoryPoints { get; }
+        public override int VictoryPoints => 2;
 
         public Golem(string name) : base(name)
         {
             this.AddItem(new Axe());
             this.AddItem(new Armor());
-            this.VictoryPoints = 1;
         }
     }
 }
