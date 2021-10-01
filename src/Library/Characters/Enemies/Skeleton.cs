6ce8c61680dd059e2ced690934a9ffc5301414fe
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2021-10-01 14:01:05 -0300

    updated Skeleton.cs

---
+++ src/Library/Characters/Enemies/Skeleton.cs
@@ -0,0 +1,14 @@
+namespace RoleplayGame
+{
+    public class Skeleton : Enemy
+    {
+        public override int VictoryPoints { get; }
+
+        public Skeleton(string name) : base(name)
+        {
+            this.VictoryPoints = 2;
+            this.AddItem(new Sword());
+        }
+
+    }
+}
