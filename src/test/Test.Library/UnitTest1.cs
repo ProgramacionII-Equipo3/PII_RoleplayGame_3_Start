c88a72dd2714f5fd7288de2718bbd701712bfd40
Juan Nocetti <juan.anocetti@gmail.com>
2021-10-02 20:06:41 -0300

    create characters in test

--- src/test/Test.Library/UnitTest1.cs
+++ src/test/Test.Library/UnitTest1.cs
@@ -14,5 +14,35 @@ namespace Test.Library
         {
             Assert.Pass();
         }
+        
+        [Test]
+        public void EncounterTest()
+        {
+            // Heroe
+            Archer arquero1 = new Archer ("Arquero");
+            arquero1.Bow = new Bow();
+            arquero1.Helmet = new Helmet();
+            
+            // Enemy
+            Cyclops ciclope1 = new Cyclops ("Cyclope");
+            ciclope1.Shield = new Shield ();
+            ciclope1.Axe = new Axe ();
+            
+            // Enemy
+            Golem golem1 = new Golem ("El golem");
+            golem1.Axe = new Axe ();
+            golem1.Armor = new Armor ();
+
+            // Heroe
+            Knight caballero1 = new Knight ("El caballero ");
+            caballero1.Sword = new Sword ();
+            caballero1.Armor = new Armor ();
+            caballero1.Shield = new Shield();
+
+            
+
+
+
+        }
     }
 }
